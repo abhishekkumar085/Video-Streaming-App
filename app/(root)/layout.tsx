7a829059bb7c486491aa1ea9074f4a8902d0801b
abhishekkumar085 <abhishek950820@gmail.com>
2024-05-05 19:47:20 +0530

    Feature :- Meeting Created Added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,15 @@
+
+import StreamVideoProvider from '@/providers/StreamClientProvider'
 import React, {  ReactNode } from 'react'
 
 const RootLayout = ({children}:{children:ReactNode}) => {
   return (
    <main>
+   <StreamVideoProvider>
+   {children} 
+   </StreamVideoProvider>
     
-{children}
+
 
    </main>
   )
