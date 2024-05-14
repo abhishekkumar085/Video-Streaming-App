cb460de955008c888a8961b2bd0f2b6ce7d30141
abhishekkumar085 <abhishek950820@gmail.com>
2024-05-14 11:01:40 +0530

    Call list Component Added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,14 @@
 
 import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
 import React, {  ReactNode } from 'react'
-
+export const metadata: Metadata = {
+  title: 'Meet together',
+  description: 'Video calling App',
+  icons:{
+    icon:'/icons/logo.svg'
+  },
+};
 const RootLayout = ({children}:{children:ReactNode}) => {
   return (
    <main>
